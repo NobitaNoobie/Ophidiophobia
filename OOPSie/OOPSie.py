bcf3529e50543eeca9c16b023cbe829e6683f3bb
NobitaNoobie <91742427+NobitaNoobie@users.noreply.github.com>
2022-01-09 17:17:20 +0530

    Create OOPSie.py

---
+++ OOPSie/OOPSie.py
@@ -0,0 +1,9 @@
+#self keyword in python is synonymous to this keyword in java
+
+class Computer:
+    def config(self):
+        print("luchi")
+
+obj1 = Computer()
+obj2 = Computer()
+Computer.config()
